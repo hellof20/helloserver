2c978e7970a6113fd5f2bdc59df367478ad1bf04
pwm <panwenmi@amazon.com>
2021-02-21 03:13:31 +0000

    20210221

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
-const webContent = "Helloserver copilot"
+const webContent = "Helloserver copilot 2022"
 
 func main() {
 	http.HandleFunc("/", helloHandler)
@@ -14,5 +18,14 @@ func main() {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, webContent)
+	cmd := exec.Command("hostname")
+	var out bytes.Buffer
+	multi := io.MultiWriter(os.Stdout, &out)
+	cmd.Stdout = multi
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Fprint(w, out.String())
 }
